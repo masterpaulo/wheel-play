645850f26cb5b7bfe7990de5e7ef0fb2b104943f
masterpaulo <dev.masterpaulo@gmail.com>
2019-02-21 15:28:38 +0800

    Improve code structure by creating subclass

---
+++ Playground/Objects/SteeringWheel.swift
@@ -0,0 +1,148 @@
+//
+//  SteeringWheel.swift
+//  Playground
+//
+//  Created by Master Paulo on 21/02/2019.
+//  Copyright © 2019 Master Paulo. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class SteeringWheel: SKSpriteNode {
+    
+    var steeringAngle: Double = 0.0
+    var state : SteeringWheelState = .stop
+    
+    private var steeringTouchNodeID: Int? = nil
+    
+    private var steeringVelocity: CGFloat = 0.0
+    
+    // constants
+    let steeringAngleLimit: Double = 450
+    let steeringVelocitycorrectionAmount = 1
+    let maxSteeringVelocity = CGFloat(5)
+    
+    
+    private var startingAngle: CGFloat?
+    private var startingTime: TimeInterval?
+    
+    
+    enum SteeringWheelState : String{
+        case hold = "steering" // grabbed angle any
+        case release = "counterSteering" // not grabbed angle not 0
+        case stop = "neutral" // not grabbed angle 0
+    }
+    
+    
+    init() {
+        // super.init(imageNamed: String) You can't do this because you are not calling a designated initializer.
+        let texture = SKTexture(imageNamed: "steering_wheel")
+        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+        self.name = "SteeringWheel"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    func moveSteeringWheel(toAngle: Double){
+        let a = CGFloat(steeringAngle).degreesToRadians()
+        let rotate = SKAction.rotate(toAngle: a, duration: 0, shortestUnitArc: true)
+        self.run(rotate)
+    }
+    
+    func updateSteeringAngle(by deltaAngle:CGFloat){
+        steeringAngle += Double(deltaAngle)
+        if ( abs( steeringAngle ) > steeringAngleLimit ) {
+            steeringAngle = (steeringAngle < 0) ? steeringAngleLimit * -1 : steeringAngleLimit;
+            steeringVelocity *= -0.1
+        }
+        moveSteeringWheel(toAngle: steeringAngle)
+    }
+    
+
+    // MARK: Touch Actions
+    
+    func touchDown(t : UITouch) {
+        let location = t.location(in:self.parent!)
+        let dx = location.x - self.position.x
+        let dy = location.y - self.position.y
+        // Store angle and current time
+        startingAngle = atan2(dy, dx).radiansToDegrees()
+        startingTime = t.timestamp
+        self.physicsBody?.angularVelocity = 0
+        steeringVelocity = 0.0
+        
+        self.state = .hold
+        steeringTouchNodeID = t.hashValue
+    }
+    
+    func touchMoved(t : UITouch) {
+        let location = t.location(in:self.parent!)
+        let node = self
+        
+        if let touchNode = steeringTouchNodeID, let startAngle = startingAngle {
+            if state == .hold && t.hashValue == touchNode {
+                let dx = location.x - node.position.x
+                let dy = location.y - node.position.y
+                
+                let angle = atan2(dy, dx).radiansToDegrees()
+                
+                // let dt = CGFloat(t.timestamp - startingTime!)
+                
+                // Calculate angular velocity; handle wrap at pi/-pi
+                let deltaAngle = angularDistanc(startAngle, angle)
+                steeringVelocity = deltaAngle
+                updateSteeringAngle(by: deltaAngle)
+                
+                // Update angle and time
+                startingAngle = angle
+                startingTime = t.timestamp
+            }
+        }
+    }
+    
+    func touchUp(t: UITouch) {
+        if let touchNode = steeringTouchNodeID, touchNode == t.hashValue {
+            self.state = .release
+            
+            steeringTouchNodeID = nil
+            startingAngle = nil
+            startingTime = nil
+        }
+    }
+    
+    // MARK: Update function
+    
+    func update(_ currentTime: TimeInterval){
+        if state == .release {
+            
+            // update steering velocity
+            if steeringAngle < 5 && steeringAngle > -5 {
+                if abs(steeringVelocity) < 10 {
+                    steeringVelocity = 0.0
+                    steeringAngle = 0.0
+                    state = .stop
+                }
+            }
+            else {
+                let correction = steeringVelocitycorrectionAmount
+                let max = maxSteeringVelocity
+                if steeringAngle > 0 {
+                    let change = steeringVelocity > -max ? -correction : correction/2
+                    steeringVelocity += CGFloat(change)
+                }
+                else {
+                    let change = steeringVelocity < max ? -correction : correction/2
+                    steeringVelocity -= CGFloat(change)
+                }
+            }
+            
+            // update steering wheel via velocity
+            updateSteeringAngle(by: steeringVelocity)
+        }
+    }
+}
---
+++ Playground/SKScenes/GameScene.swift
@@ -0,0 +1,111 @@
+//
+//  GameScene.swift
+//  Playground
+//
+//  Created by Master Paulo on 18/02/2019.
+//  Copyright © 2019 Master Paulo. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var spinnyNode : SKShapeNode?
+    
+    
+    var steeringWheel: SteeringWheel = SteeringWheel()
+    var steeringIndicator: SKSpriteNode = SKSpriteNode()
+    var steeringAngleLabel: SKLabelNode = SKLabelNode()
+    
+    
+    override func didMove(to view: SKView) {
+
+        // Create shape node to use during mouse interaction
+        let w = (self.size.width + self.size.height) * 0.05
+        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        
+        if let spinnyNode = self.spinnyNode {
+            spinnyNode.lineWidth = 2.5
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),SKAction.fadeOut(withDuration: 0.5),SKAction.removeFromParent()]))
+        }
+        
+        let h = UIScreen.main.bounds.height
+        steeringWheel = SteeringWheel()
+        steeringWheel.position = CGPoint(x: 0, y: (-h/2 + (h * 0)))
+        steeringWheel.setScale(1.5)
+        self.addChild(steeringWheel)
+        
+        if let indicator = self.childNode(withName: "SteeringIndicator") as? SKSpriteNode{
+            steeringIndicator = indicator
+        }
+        
+        if let label = self.childNode(withName: "SteeringAngleLabel") as? SKLabelNode{
+            steeringAngleLabel = label
+        }
+    }
+    
+    
+    func touchDown(t : UITouch) {
+        let location = t.location(in:self)
+        let node = atPoint(location)
+        if let wheel = node as? SteeringWheel {
+            wheel.touchDown(t: t)
+        }
+    }
+    
+    func touchMoved(t : UITouch) {
+        if steeringWheel.state == .hold {
+            steeringWheel.touchMoved(t: t)
+        }
+    }
+    
+    func touchUp(t: UITouch) {
+        if steeringWheel.state == .hold {
+            steeringWheel.touchUp(t: t)
+        }
+    }
+    
+   
+    // MARK: Overrides
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(t:t) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(t: t) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(t:t) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(t:t) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+        if(steeringWheel.state != .stop){
+            let steeringAngle = steeringWheel.steeringAngle
+            
+            steeringWheel.update(currentTime)
+            moveSteeringIndicator(to: steeringAngle)
+            steeringAngleLabel.text = String(steeringAngle.rounded())
+        }
+        
+        
+        // the last time interval that the frame updated
+        //lastTime = currentTime
+    }
+    
+    func moveSteeringIndicator(to steeringAngle:Double){
+        let limit = steeringWheel.steeringAngleLimit
+        let pos = CGFloat( ( steeringAngle / limit) * -300)
+        let move = SKAction.moveTo(x: pos, duration: 0)
+        steeringIndicator.run(move)
+    }
+}
